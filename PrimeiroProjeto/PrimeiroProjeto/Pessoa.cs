231e22046bec60ca762dbc91adbff17183755a4f
Genilson <genisnilson@gmail.com>
2022-03-09 15:28:39 -0300

    Initial commit

---
+++ PrimeiroProjeto/PrimeiroProjeto/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿namespace PrimeiroProjeto
+{
+    public class Pessoa
+    {
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+
+        public int Idade { get; private set; }
+
+        public Pessoa(string nome, string sobrenome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sobrenome = sobrenome;
+        }
+
+        public string NomeCompleto() => Nome + " " + Sobrenome;
+
+    }
+}
---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+            Pessoa novaPessoa = new Pessoa("João Victor", 21);
+
+            Console.WriteLine("Nome: " + novaPessoa.ObterNome());
+            Console.WriteLine("Idade: " + novaPessoa.ObterIdade());
+            */
+
+            Pessoa novaPessoa = new Pessoa("João", "Victor", 21);
+
+            Console.WriteLine("Idade: " + novaPessoa.Nome);
+            Console.WriteLine(novaPessoa.NomeCompleto());
+        }
+    }
+}
