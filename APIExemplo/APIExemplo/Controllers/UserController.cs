9f70d779718aded8244dc729edd2130e8645ea2c
genilson.martins <genilson.martins@virtus.ufcg.edu.br>
2024-04-12 18:41:53 -0300

    WebApi exemplo

---
+++ APIExemplo/APIExemplo/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using APIExemplo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIExemplo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping() => "pong";
+
+        [HttpGet("list")]
+        public User GetUser()
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Maria da Silva",
+                Age = 30
+            };
+        }
+
+        [HttpPost]
+        public ActionResult Register([FromBody] User user)
+        {
+            //return NotFound();
+            //return Unauthorized();
+            //return NoContent();
+            //Cadastrar user
+            return Created(
+                "http://localhost:5278/api/user/list", 
+                user);
+        }
+    }
+}
